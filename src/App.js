add9599e068f35ad60cae289bc1152fc00694d4d
pki071120 <ddametyy@gmail.com>
2024-12-17 08:16:51 +0900

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./GlobalStyle";
-import Home from "./home";
+import { Sub1, Sub2, Sub3, Check } from "./page";
+import { useState } from "react";
 
 function App() {
+	const [isMuted, setIsMuted] = useState(false);
+	const [isTransitioning, setIsTransitioning] = useState(false);
 	return (
 		<>
 			<GlobalStyle />
 			<div className="App">
-				<Home />
+				<BrowserRouter>
+					<Routes>
+						<Route
+							path="/"
+							element={
+								<Check
+									setIsMuted={setIsMuted}
+									isTransitioning={isTransitioning}
+									setIsTransitioning={setIsTransitioning}
+								/>
+							}
+						/>
+						<Route path="/main" element={<Sub1 />} />
+						<Route path="/trailer" element={<Sub2 isMuted={isMuted} />} />
+						<Route path="/villages" element={<Sub3 />} />
+					</Routes>
+				</BrowserRouter>
 			</div>
 		</>
 	);
