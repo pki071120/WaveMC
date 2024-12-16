fb0603c61e557d32dc53e2b68f9fd1eeb4933cc8
pki071120 <ddametyy@gmail.com>
2024-12-17 08:39:02 +0900

    Edit SubPage Navigation

--- src/components/Cover/index.jsx
+++ src/components/Cover/index.jsx
@@ -19,19 +19,22 @@ const Cover = ({ navigation }) => {
 			<S.SubPagesWrapper>
 				<span onClick={() => navigation("/main")}>
 					<h2>[개척하라]</h2>
-					<Fade Des={false} />
-				</span>
-				<span onClick={() => navigation("/trailer")}>
-					<h2>[토벌하라]</h2>
 					<img
 						src={`https://4r1tfd9ccoygbrkd.public.blob.vercel-storage.com/img/imageSub2-n443WrnSxclIyTnZHl8VAveUdqNZWI.png`}
 						alt="Thumbnail1"
 					/>
 				</span>
+				<span onClick={() => navigation("/trailer")}>
+					<h2>[토벌하라]</h2>
+					<img
+						src={`https://4r1tfd9ccoygbrkd.public.blob.vercel-storage.com/img/imageSub3-fKXymvWrPlDRkJExsdQul21RIz40Le.png`}
+						alt="Thumbnail2"
+					/>
+				</span>
 				<span onClick={() => navigation("/villages")}>
 					<h2>[함께하라]</h2>
 					<img
-						src={`https://4r1tfd9ccoygbrkd.public.blob.vercel-storage.com/img/imageSub3-fKXymvWrPlDRkJExsdQul21RIz40Le.png`}
+						src={`https://4r1tfd9ccoygbrkd.public.blob.vercel-storage.com/img/village1-yW2WgVBN1n0ZakdWws7AstMzOKGLmf.png`}
 						alt="Thumbnail2"
 					/>
 				</span>
