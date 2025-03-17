e512abfa9b38ac1b32b199e997fe47c6b2d8c1ed
pki071120 <ddametyy@gmail.com>
2025-03-17 20:05:45 +0900

    reset

--- src/components/Fade/style.js
+++ src/components/Fade/style.js
@@ -2,72 +2,6 @@ import styled from "@emotion/styled";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-// 전체 컨테이너
-export const Container = styled.div`
-	width: 100vw;
-	height: 100vh;
-	z-index: 2;
-	position: absolute;
-`;
-
-// 로고 위치
-export const Logo = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	width: 70px;
-	height: 70px;
-	position: absolute;
-	top: 2rem;
-	right: 2rem;
-`;
-
-// Discord 링크 버튼
-export const DiscordLink = styled.a`
-	display: flex;
-	align-items: center;
-	width: 50px;
-	height: 50px;
-	z-index: 1;
-	position: absolute;
-	bottom: 2rem;
-	left: 2rem;
-
-	@media (max-width: 900px) {
-		top: 2.5rem;
-		left: auto;
-		right: 6rem;
-	}
-
-	@media (max-width: 375px) {
-		right: 4rem;
-	}
-`;
-
-// 사운드 버튼
-export const Sound = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	width: 4rem;
-	height: 4rem;
-	position: absolute;
-	bottom: 1rem;
-	right: 1rem;
-	cursor: pointer;
-	z-index: 3;
-
-	@media (max-width: 900px) {
-		top: 2rem;
-		right: 9rem;
-	}
-
-	@media (max-width: 375px) {
-		right: 7rem;
-	}
-`;
-
-// 페이지 Wrapper
 export const Wrapper = styled.div`
 	width: 100vw;
 	height: 100vh;
@@ -75,7 +9,6 @@ export const Wrapper = styled.div`
 	z-index: 2;
 `;
 
-// 각 아이템
 export const Item = styled.div`
 	display: flex;
 	justify-content: center;
@@ -84,7 +17,6 @@ export const Item = styled.div`
 	height: 100vh;
 `;
 
-// 이미지 스타일
 export const Img = styled.img`
 	width: 100vw;
 	height: 100vh;
@@ -94,7 +26,6 @@ export const Img = styled.img`
 			: "object-fit : cover; object-position: center;"};
 `;
 
-// 설명 박스 스타일
 export const Description = styled.div`
 	display: flex;
 	position: absolute;
@@ -124,39 +55,71 @@ export const Description = styled.div`
 		left: auto;
 	}
 
-	@media (max-width: 375px) {
-		bottom: 5rem;
-	}
-`;
+	div {
+		display: flex;
+		flex-direction: column;
+		gap: 1.5rem;
+		width: 32rem;
+		padding: 2rem;
+		${({ idx }) => {
+			switch (idx) {
+				case 0:
+					return "background: rgba(0,0,0, 0.1);";
+				case 1:
+					return "background: rgba(200,200,200, 0.1);";
+				case 2:
+					return "background: rgba(256,256,256, 0.1);";
+				default:
+					return "";
+			}
+		}}
 
-// 설명 박스 내부 스타일
-export const DescriptionBox = styled.div`
-	display: flex;
-	flex-direction: column;
-	gap: 1.5rem;
-	width: 32rem;
-	padding: 2rem;
-	${({ idx }) => {
-		switch (idx) {
-			case 0:
-				return "background: rgba(0,0,0, 0.1);";
-			case 1:
-				return "background: rgba(200,200,200, 0.1);";
-			case 2:
-				return "background: rgba(256,256,256, 0.1);";
-			default:
-				return "";
+		@media (max-width: 430px) {
+			width: 80vw;
+			padding: 0.5rem;
+			gap: 0.8rem;
 		}
-	}}
 
-	@media (max-width: 430px) {
-		width: 80vw;
-		padding: 0.5rem;
-		gap: 0.8rem;
-	}
+		h2 {
+			font-size: 2.2rem;
+			${({ idx, type }) => {
+				switch (idx) {
+					case 0:
+						return type === "boss" ? "color: #9acfff;" : "color: #e9e987;";
+					case 1:
+						return type === "boss" ? "color: #afaaff;" : "color: #1c9f;";
+					case 2:
+						return type === "boss" ? "color: #fff;" : "color:  #dddddd;";
+					default:
+						return "";
+				}
+			}}
 
-	@media (max-width: 375px) {
-		width: 85vw;
-		padding: 0.3rem;
+			@media (max-width: 430px) {
+				font-size: 1rem;
+			}
+		}
+		p {
+			font-size: 1rem;
+			letter-spacing: 3px;
+			line-height: 1.1rem;
+			${({ idx, type }) => {
+				switch (idx) {
+					case 0:
+						return type === "boss" ? "color: #aacaff;" : "color: #cdcd7d;";
+					case 1:
+						return type === "boss" ? "color: #cfc0f0;" : "color: #1a9f;";
+					case 2:
+						return type === "boss" ? "color: #efefef;" : "color: #aaaaaa;";
+					default:
+						return "";
+				}
+			}}
+			@media (max-width: 430px) {
+				font-size: 0.7rem;
+				line-height: 1rem;
+				letter-spacing: 1px;
+			}
+		}
 	}
 `;
